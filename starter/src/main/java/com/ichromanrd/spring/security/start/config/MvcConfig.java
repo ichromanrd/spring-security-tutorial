fcd47f24a52a8072e5f1b778a02316e3e6c02503
ichromanrd <ird310594>
2018-11-12 07:34:38 +0700

    modify pom.xml: + update project description + disable security dependencies + add thymeleaf dependency
    add hello.html
    
    add MvcConfig.java

---
+++ starter/src/main/java/com/ichromanrd/spring/security/start/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.ichromanrd.spring.security.start.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hello").setViewName("hello");
+    }
+}
