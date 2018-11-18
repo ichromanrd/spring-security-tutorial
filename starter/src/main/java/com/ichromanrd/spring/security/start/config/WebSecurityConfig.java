f4b34bd2afdbd1c0ff6a444a901b9f20d325ace8
ichromanrd <ird310594>
2018-11-19 04:53:23 +0700

    add WebSecurityConfig.java to provide basic authentication and authorization

---
+++ starter/src/main/java/com/ichromanrd/spring/security/start/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ichromanrd.spring.security.start.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password( getPasswordEncoder().encode("password") )
+                .roles("USER");
+    }
+}
